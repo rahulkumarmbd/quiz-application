5bdabea9f4453aa620003fe416c4ef0b07002564
Rahul <rahulkumar90270@gmail.com>
2023-08-31 19:23:39 +0530

    initial ui for quiz app

--- src/app/pages/index.jsx
+++ src/app/pages/index.jsx
@@ -1,5 +1,14 @@
+import Card from "../components/common/Card";
+import Header from "../components/common/Header";
+import "./index.css";
+
 function App() {
-  return <div>Rahul singh</div>;
+  return (
+    <div className="appContainer">
+      <Header>Quiz Application UI</Header>
+      <Card>hello</Card>
+    </div>
+  );
 }
 
 export default App;
